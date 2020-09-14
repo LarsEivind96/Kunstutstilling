4150772544ade9789423b38e023df87d5da5fe65
Haakon Flaaronning <haakon@flaaronning.net>
2020-09-14 10:23:00 +0200

    Remove unused CSS and Router element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Home() {
   return (
-    <Router>
-      <div className="container">
-        <h1>Home</h1>
-      </div>
-    </Router>
+    <div className="container">
+      <h1>Home</h1>
+    </div>
   );
 }
 
