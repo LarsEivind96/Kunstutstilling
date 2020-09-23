55c080ecc95d6c30ad5578cd1fceb9739380930e
Haakon Flaaronning <haakon@flaaronning.net>
2020-09-23 14:21:25 +0200

    Fetch random text upon loading an SVG

--- src/components/SvgCard/SvgCard.tsx
+++ src/components/SvgCard/SvgCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SvgCard.css";
 
 interface Props {
@@ -6,6 +6,29 @@ interface Props {
 }
 
 const SvgCard = ({ currentSvg }: Props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchedText, setFetchedText] = useState([]);
+
+  const fetchTestText = async () => {
+    setIsLoading(true);
+    await fetch("https://poetrydb.org/random")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setFetchedText(result.lines.reduce((prev, curr, index) => [prev, <br key={index} />, curr]));
+        },
+        (error) => {
+          console.log(error);
+          return;
+        }
+      );
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTestText();
+  }, []);
+
   return (
     <div className="SvgCard">
       <div className="SvgContainer">
@@ -14,7 +37,8 @@ const SvgCard = ({ currentSvg }: Props) => {
       <div className="TextContainer">
         <div className="ImageText">
           <h1>{currentSvg.name}</h1>
-          <p>testing testing</p>
+          {isLoading && <p> Fetching poetry... </p>}
+          {!isLoading && <p> {fetchedText}</p>}
         </div>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SvgCard from "../components/SvgCard/SvgCard";
 import Svgs from "../svg/Svgs";
 
